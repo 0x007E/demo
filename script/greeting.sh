92531ffa8ef90fb665727c9a5bc9968e16706218
0x007E <raffael@sunriax.at>
2024-10-27 20:55:19 +0100

    Add user input

--- script/greeting.sh
+++ script/greeting.sh
@@ -5,3 +5,7 @@ date=$(date '+%Y-%m-%d %H:%M:%S')
 echo -e "\n\rWillkommen auf dem Rechner ..."
 echo -e "Heute ist $date\n\r"
 
+echo -n "Wie geht es dir heute? "
+read INPUT
+
+echo -e "\n\rDu hast $INPUT eingegeben!\n\r"
